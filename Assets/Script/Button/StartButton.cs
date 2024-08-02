656b3ea8b9b9d3945fca9554e484b5757cb14160
Hadi Firdaus <hadi97firdaus@gmail.com>
2024-08-02 15:28:15 +0800

    Initialize commit first time

---
+++ Assets/Script/Button/StartButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class StartButton : MonoBehaviour{
+
+	public string Level;
+	public GameObject Button;
+	private Color myColor;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void StartGame(){
+		Debug.Log ("Start Level");
+		SceneManager.LoadScene (Level);
+	}
+
+	public void GameOver(){
+		Debug.Log ("Game Over");
+		Application.Quit();
+	}
+
+	public void PointerEnter(){
+		Button.GetComponent<Image> ().color = Color.blue;
+	}
+
+	public void PointerExit(){
+		myColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+		Button.GetComponent<Image> ().color = myColor;
+	}
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	public float jump=10f;
+
+	public Rigidbody2D rb;
+	public SpriteRenderer sr;
+
+	public Color colorCyan;
+	public Color colorRed;
+	public Color colorChocolate;
+	public Color colorYellow;
+
+	public string currentColour;
+
+	void Start(){
+		RandomColour ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Jump") || Input.GetMouseButtonDown (0)) {
+			rb.velocity = Vector2.up * jump;
+		}
+
+		Exit ();
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		//Debug.Log (col.tag);
+
+		if(col.tag == "ColourChanger"){	//change colour of player when hit colourChanger
+
+			RandomColour ();
+			Destroy (col.gameObject);	//destroy colourChanger
+			return;	//wont game over
+		}
+
+		if (col.tag != currentColour) {	//langar warna bertentangan
+			Debug.Log ("Game Over!");
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		}
+	}
+
+	public void RandomColour(){
+		int index = Random.Range (0, 3);
+
+		switch (index) {
+		case 0:
+			currentColour = "Cyan";
+			sr.color = colorCyan;
+			break;
+		case 1:
+			currentColour = "Red";
+			sr.color = colorRed;
+			break;
+		case 2:
+			currentColour = "Chocolate";
+			sr.color = colorChocolate;
+			break;
+		case 3:
+			currentColour = "Yellow";
+			sr.color = colorYellow;
+			break;
+		}
+	}
+
+	void Exit(){
+		if (Input.GetKey(KeyCode.Escape)) {
+			//Debug.Log ("Quit game");
+			Application.Quit();
+		}
+	}
+}
